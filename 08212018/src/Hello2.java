5d35301966e7db13d37a5f9a4069ad062d790652
Neil <neilbalch@gmail.com>
2018-08-21 12:59:39 -0700

    Replace IDEA files with vscode files

--- 08212018/src/Hello2.java
+++
@@ -1,7 +0,0 @@
-public class Hello2 {
-    public static void main(String[] args) {
-        String name = "Neil";
-
-        System.out.println("Hello, " + name);
-    }
-}
--- 08212018/src/Hello3.java
+++
@@ -1,7 +0,0 @@
-public class Hello3 {
-    public static void main(String[] args) {
-        String favoriteColor = "Orange";
-
-        System.out.println("My favorite color is: " + favoriteColor);
-    }
-}
