16d051d38ac893571f4fb313e7da593af6acb79f
Neil <neilbalch@gmail.com>
2018-08-27 08:59:53 -0700

    Classwork for Aug 27, 2018

---
+++ 08272018/Identifiers.java
@@ -0,0 +1,21 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+
+//indentifier example
+
+public class Identifiers {
+    public static void main(String[] args) {
+        int big_Num = 99;
+
+        double ldecimal = 8.25;
+
+        double something_else = 657;
+
+        char littleA = 'a';
+
+        boolean isPrime = false;
+
+        String s = "abc";
+
+        System.out.println(s);
+    }
+}
---
+++ 08272018/VariableTest.java
@@ -0,0 +1,9 @@
+public class VariableTest {
+    public static void main(String[] args) {
+        int integer = 111;
+        double double_precision = 3.14;
+        char character = 'C';
+
+        System.out.println(integer + " " + double_precision + " " + character);
+    }
+}
