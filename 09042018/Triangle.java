7107c5163ba5da6e0a2dc439dda2701cb9ed3b86
Neil <neilbalch@gmail.com>
2018-09-04 08:52:53 -0700

    Classwork for Sept 4, 2018

---
+++ 09042018/Triangle.java
@@ -0,0 +1,9 @@
+public class Triangle {
+    public static void main(String[] args) {
+        double base = 6.0;
+        double height = 10.0;
+        double area = 0.5 * base * height;
+
+        System.out.println("A triangle with base=" + base + " and a height=" + height + " has the area=" + area);
+    }
+}
