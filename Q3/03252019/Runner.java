d40e1b15c15541f5f52102e0d8b3b46d3ee5fd18
Neil <neilbalch@gmail.com>
2019-03-25 20:28:00 -0700

    Classwork from March 25, 2019

---
+++ Q3/03252019/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        Cat c = new Cat();
+
+        System.out.println(d.speak());
+        System.out.println(c.speak());
+    }
+}
