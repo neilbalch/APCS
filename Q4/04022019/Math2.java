ead010ba86eda4102d6877436d6ada9d2bba05e7
Neil <neilbalch@gmail.com>
2019-04-02 08:18:24 -0700

    Classwork from April 2, 2019, refactored to fit with new quarter

---
+++ Q4/04022019/Math2.java
@@ -0,0 +1,5 @@
+public class Math2 {
+    public static double getTriangleArea(double base, double height) { return 0.5 * base * height; }
+
+    public static double getC(double a, double b) { return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)); }
+}
---
+++ Q4/04022019/Runner.java
@@ -0,0 +1,6 @@
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(Math2.getTriangleArea(5.0, 6.0));
+        System.out.println(Math2.getC(6.0, 8.0));
+    }
+}
